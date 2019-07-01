7c27c6c12a894fda6bfff37557d369d2c020fdab
tampler <socnetfpga@gmail.com>
2019-07-01 17:22:35 +0300

    Implemented subscribe-poll

--- src/main/scala/zio/kafka/client/ConsumerTop.scala
+++ src/main/scala/zio/kafka/client/ConsumerTop.scala
@@ -3,13 +3,20 @@ package kafkaconsumer
 import org.apache.kafka.common.serialization.{ Serde, Serdes }
 import org.apache.kafka.clients.consumer.ConsumerConfig
 
-import zio.{ Chunk, DefaultRuntime, Task, TaskR, ZIO }
+import zio.{ Chunk, DefaultRuntime, TaskR }
 import zio.blocking.Blocking
 import zio.clock.Clock
 import zio.duration._
 
 import zio.kafka.client.{ Consumer, ConsumerSettings, Subscription }
-import zio.kafka.client.KafkaTestUtils._
+import zio.kafka.client.KafkaTestUtils.{ pollNtimes }
+
+final case class ConnectionSetup(
+  server: String,
+  client: String,
+  group: String,
+  topic: String
+)
 
 sealed abstract class KafkaConsumer extends DefaultRuntime {
 
@@ -17,9 +24,11 @@ sealed abstract class KafkaConsumer extends DefaultRuntime {
 
   def settings(server: String, groupId: String, clientId: String): ConsumerSettings
   def run[A](server: String, groupId: String, clientId: String)(r: WorkerType[A]): A
-  def subscribe(server: String, groupId: String, clientId: String, topic: String): Task[Unit]
-  def unsubscribe(server: String, groupId: String, clientId: String): Task[Unit]
-  def poll(server: String, groupId: String, clientId: String, topic: String): Chunk[String]
+  // def subscribe(server: String, groupId: String, clientId: String, topic: String): Task[Unit]
+  // def unsubscribe(server: String, groupId: String, clientId: String): Task[Unit]
+  // def poll(server: String, groupId: String, clientId: String, topic: String): Chunk[String]
+
+  def readBatch(cfg: ConnectionSetup): Chunk[String]
 
 }
 
@@ -41,7 +50,7 @@ object KafkaConsumer extends KafkaConsumer {
       Consumer.make[String, String](settings(server, groupId, clientId)).use(r)
     )
 
-  def subscribe(server: String, groupId: String, clientId: String, topic: String): Task[Unit] =
+  /* def subscribe(server: String, groupId: String, clientId: String, topic: String): Task[Unit] =
     run(server, groupId, clientId) { consumer =>
       for {
         outcome <- consumer.subscribe(Subscription.Topics(Set(topic)))
@@ -62,5 +71,16 @@ object KafkaConsumer extends KafkaConsumer {
         recs <- pollNtimes(10, consumer)
         tmp  = recs.map(_.value)
       } yield tmp
+    } */
+
+  def readBatch(cfg: ConnectionSetup): Chunk[String] =
+    run(cfg.server, cfg.group, cfg.client) { consumer =>
+      (consumer.subscribe(Subscription.Topics(Set(cfg.topic))) *> pollNtimes(10, consumer)).map { recs =>
+        for {
+          tmp <- recs.map(_.value)
+        } yield tmp
+      }
+
     }
+
 }
